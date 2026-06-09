9959472d8f1a72c7ccdf9c5ff2f233627f3fd4d1
denis <denis.krisevich@gmail.com>
2026-06-09 10:09:46 +0300

    feat: favicon adding

--- config/build/buildPlugins.js
+++ config/build/buildPlugins.js
@@ -5,6 +5,7 @@ export function buildPlugins(options) {
     const plugins = [
         new HtmlWebpackPlugin({
             template: options.paths.html,
+            favicon: "./public/favicon.ico"
         })
     ]
 
